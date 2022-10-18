58fa784e686771648a5a34c6337327d0ad2d7e01
Missael Moreno <m_moreno.15@hotmail.com>
2022-10-18 09:59:28 -0500

    Initial

--- efistub.sh
+++ efistub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-source /home/miscawelo/scripts/efistub/efistub.conf
+source /install_scripts/efistub.conf
 
 # Root partition
 root_uuid=$(sudo blkid | grep LUKS | cut -d '"' -f 2)
@@ -10,38 +10,29 @@ boot_partition="$(lsblk -l | grep boot | cut -d ' ' -f 1)"
 boot_drive="/dev/"${boot_partition%p*}
 boot_part=${boot_partition#*p}
 
-# Swap and hibernation
-swap_uuid=$(grep root -A1 /etc/fstab | tail -n 1 |  sed -e 's/.*UUID=//' | cut -d $'\t' -f 1)
-swap_offset=$(sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}')
-hibernation="resume=UUID="$swap_uuid" resume-offset="$swap_offset
-
 # LUKS options
 luks_options="rd.luks.options="$luks_options
 
 sudo rm /tmp/cmd_line &> /dev/null
-echo "root=UUID="$root_uuid" rw "$luks_options" "$hibernation" "$kernel_params > /tmp/cmd_line
+echo "root=UUID="$root_uuid" rw "$luks_options" "$kernel_params > /tmp/cmdline
 
-for linuz in $efi_dir/vmlinuz*; do
-    kernel=${linuz#*-}
-    echo "Creating unified kernel image for $kernel"
-    sudo rm /tmp/$kernel &> /dev/null
-    cat $efi_dir/$ucode $efi_dir/booster-$kernel.img > /tmp/$kernel
-    objcopy \
-        --add-section .osrel="/usr/lib/os-release" --change-section-vma .osrel=0x20000 \
-        --add-section .cmdline="/tmp/cmd_line" --change-section-vma .cmdline=0x30000 \
-        --add-section .linux="$linuz" --change-section-vma .linux=0x2000000 \
-        --add-section .initrd="/tmp/$kernel" --change-section-vma .initrd=0x3000000 \
-        "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" "$kernel.efi"
-    sudo cp $kernel.efi $efi_dir/EFI/Linux
-    sudo rm $kernel.efi &> /dev/null
+cat /boot/amd-ucode.img /boot/booster-linux.img > /tmp/linux.img
 
-    entry=$(efibootmgr | grep "($kernel)" | grep -oP "(?<=Boot).*(?=\*)")
-    if [[ ! -z $entry ]]; then
-        for i in $(echo $entry); do
-            sudo efibootmgr -Bb "$i" &> /dev/null
-        done
-    fi
-    sudo efibootmgr --create --disk $boot_drive --part $boot_part --label "$distro ($kernel)" --loader 'EFI\Linux\'$kernel.efi &> /dev/null
-done
+stub_line=$(objdump -h "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" | tail -2 | head -1)
+stub_size=0x$(echo "$stub_line" | awk '{print $3}')
+stub_offs=0x$(echo "$stub_line" | awk '{print $4}')
+osrel_offs=$((stub_size + stub_offs))
+cmdline_offs=$((osrel_offs + $(stat -c%s "/usr/lib/os-release")))
+splash_offs=$((cmdline_offs + $(stat -c%s "/tmp/cmdline")))
+linux_offs=$((splash_offs + $(stat -c%s "/usr/share/systemd/bootctl/splash-arch.bmp")))
+initrd_offs=$((linux_offs + $(stat -c%s "/boot/vmlinuz-linux")))
 
-sudo sbctl sign-all
+objcopy \
+    --add-section .osrel="/usr/lib/os-release" --change-section-vma .osrel=$(printf 0x%x $osrel_offs) \
+    --add-section .cmdline="/tmp/cmdline" --change-section-vma .cmdline=$(printf 0x%x $cmdline_offs) \
+    --add-section .splash="/usr/share/systemd/bootctl/splash-arch.bmp" --change-section-vma .splash=$(printf 0x%x $splash_offs) \
+    --add-section .linux="/boot/vmlinuz-linux" --change-section-vma .linux=$(printf 0x%x $linux_offs) \
+    --add-section .initrd="/tmp/linux.img" --change-section-vma .initrd=$(printf 0x%x $initrd_offs) \
+    "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" "linux.efi"
+
+    sudo efibootmgr --create --disk $boot_drive --part $boot_part --label "$distro" --loader 'EFI\Linux\'linux.efi &> /dev/null
--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
-# install_scripts
+#!/usr/bin/bash
 
 stub_line=$(objdump -h "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" | tail -2 | head -1)
-$ stub_size=0x$(echo "$stub_line" | awk '{print $3}')
-$ stub_offs=0x$(echo "$stub_line" | awk '{print $4}')
-$ osrel_offs=$((stub_size + stub_offs))
-$ cmdline_offs=$((osrel_offs + $(stat -c%s "/usr/lib/os-release")))
-$ splash_offs=$((cmdline_offs + $(stat -c%s "/etc/kernel/cmdline")))
-$ linux_offs=$((splash_offs + $(stat -c%s "/usr/share/systemd/bootctl/splash-arch.bmp")))
-$ initrd_offs=$((linux_offs + $(stat -c%s "vmlinuz-file")))
-$ objcopy \
+stub_size=0x$(echo "$stub_line" | awk '{print $3}')
+stub_offs=0x$(echo "$stub_line" | awk '{print $4}')
+osrel_offs=$((stub_size + stub_offs))
+cmdline_offs=$((osrel_offs + $(stat -c%s "/usr/lib/os-release")))
+splash_offs=$((cmdline_offs + $(stat -c%s "/etc/kernel/cmdline")))
+linux_offs=$((splash_offs + $(stat -c%s "/usr/share/systemd/bootctl/splash-arch.bmp")))
+initrd_offs=$((linux_offs + $(stat -c%s "vmlinuz-file")))
+objcopy \
     --add-section .osrel="/usr/lib/os-release" --change-section-vma .osrel=$(printf 0x%x $osrel_offs) \
-    --add-section .cmdline="/etc/kernel/cmdline" \
+    --add-section .cmdline="/install_scripts/cmdline" \
     --change-section-vma .cmdline=$(printf 0x%x $cmdline_offs) \
     --add-section .splash="/usr/share/systemd/bootctl/splash-arch.bmp" \
     --change-section-vma .splash=$(printf 0x%x $splash_offs) \
     --add-section .linux="vmlinuz-file" \
     --change-section-vma .linux=$(printf 0x%x $linux_offs) \
-    --add-section .initrd="initrd-file" \
+    --add-section .initrd="/tmp/linux.img" \
     --change-section-vma .initrd=$(printf 0x%x $initrd_offs) \
     "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" "linux.efi"
